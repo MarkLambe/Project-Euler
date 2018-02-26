c523c828d856755ab75548ce7060403e97f10060
Mark Lambe <Mark@MarkLambe.com>
2018-02-26 23:32:42 +0000

    tweak

--- problem18.js
+++ problem18.js
@@ -18,7 +18,6 @@ let data = [
 
 function solve(triangle){
     for (let i = triangle.length - 2; i >= 0; i--) {
-        let curr = 0;
         for (let j = 0; j <= i; j++) {
             triangle[i][j] += Math.max(triangle[i+1][j], triangle[i+1][j+1]);
         }
@@ -26,7 +25,7 @@ function solve(triangle){
     return triangle;
 }
 
-solve(data)[0][0];
+console.log(solve(data)[0][0]);
 
 
 
