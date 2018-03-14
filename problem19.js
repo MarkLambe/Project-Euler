9acf46d4136f7887116f7f22b8cc8cbc8cb166ce
Mark Lambe <Mark@MarkLambe.com>
2018-03-14 22:39:53 +0000

    19 done

--- problem19.js
+++ problem19.js
@@ -1,9 +1,24 @@
 daysInMonths = [31,28,31,30,31,30,31,31,30,31,30,31]
 
-for(let year = 1901;year <= 2000;year++){
+let total = 0;
+
+let dayOfTheWeek = 1;
+for(let year = 1900;year <= 2000;year++){
+  
   for(let month = 0;month <= 11;month++){
-    for(let day = 1;day <= daysInMonths[month];day++){
-      
+    let leapYearDay = 0;
+    if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0) && month == 1){
+      leapYearDay = 1;
+    }
+    for(let day = 0;day < daysInMonths[month] + leapYearDay;day++){
+      if(day == 0 && dayOfTheWeek == 0 && year >= 1901){
+        total++;
+      }
+      dayOfTheWeek++;
+      if(dayOfTheWeek == 7){
+        dayOfTheWeek = 0;
+      }
     }
   }
 }
+console.log(total);
