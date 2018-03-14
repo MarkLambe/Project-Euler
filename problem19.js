b215a8baa40e3ac07c708dfe110751ff24c63a01
Mark Lambe <Mark@MarkLambe.com>
2018-03-14 22:19:56 +0000

    started

---
+++ problem19.js
@@ -0,0 +1,9 @@
+daysInMonths = [31,28,31,30,31,30,31,31,30,31,30,31]
+
+for(let year = 1901;year <= 2000;year++){
+  for(let month = 0;month <= 11;month++){
+    for(let day = 1;day <= daysInMonths[month];day++){
+      
+    }
+  }
+}
